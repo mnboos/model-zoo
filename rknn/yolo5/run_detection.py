30b1ab5e973e9cb2ffafc2ec0275c292dc2edd4a
Martin Boos <mboos@outlook.com>
2025-08-21 10:40:01 +0200

    fix inference script

--- rknn/yolo5/run_detection.py
+++ rknn/yolo5/run_detection.py
@@ -239,32 +239,17 @@ if __name__ == '__main__':
 
     # pre-process config
     print('--> Config model')
-    rknn.config(mean_values=[[0, 0, 0]], std_values=[[255, 255, 255]], target_platform='rk3566')
+    #rknn.config(mean_values=[[0, 0, 0]], std_values=[[255, 255, 255]], target_platform='rk3566')
     print('done')
 
     # Load ONNX model
     print('--> Loading model')
-    ret = rknn.load_onnx(model=ONNX_MODEL)
+    ret = rknn.load_rknn("./out/yolov5s_relu.rknn")
     if ret != 0:
         print('Load model failed!')
         exit(ret)
     print('done')
 
-    # Build model
-    print('--> Building model')
-    ret = rknn.build(do_quantization=QUANTIZE_ON, dataset=DATASET)
-    if ret != 0:
-        print('Build model failed!')
-        exit(ret)
-    print('done')
-
-    # Export RKNN model
-    print('--> Export rknn model')
-    ret = rknn.export_rknn(RKNN_MODEL)
-    if ret != 0:
-        print('Export rknn model failed!')
-        exit(ret)
-    print('done')
 
     # Init runtime environment
     print('--> Init runtime environment')
